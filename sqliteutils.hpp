cc415f4b13d91d55303e853c880d3dee7c626c83
janezz55 <janezz55@gmail.com>
2016-03-04 10:50:45 +0100

    some fixes

--- sqliteutils.hpp
+++ sqliteutils.hpp
@@ -698,13 +698,71 @@ void foreach_stmt(stmt_t const& stmt, F&& f) noexcept(noexcept(f()))
   }
 }
 
-//emplace_back////////////////////////////////////////////////////////////////
+//emplace/////////////////////////////////////////////////////////////////////
 template <typename C>
-inline void emplace_back(stmt_t const& stmt, C& c, int const i = 0)
+inline void emplace(stmt_t const& stmt, C& c, int const i = 0)
 {
+  decltype(exec(stmt)) r(SQLITE_DONE);
+
   for (;;)
   {
-    switch (exec(stmt))
+    switch (r = exec(stmt))
+    {
+      case SQLITE_ROW:
+        c.emplace(get<typename C::value_type>(stmt, i));
+
+        continue;
+
+      case SQLITE_DONE:
+        break;
+
+      default:
+        assert(!"unhandled result from exec");
+    }
+
+    break;
+  }
+
+  return r;
+}
+
+//emplace_n///////////////////////////////////////////////////////////////////
+template <typename C, typename T>
+inline auto emplace_n(stmt_t const& stmt, C& c, T const n, int const i = 0)
+{
+  decltype(exec(stmt)) r(SQLITE_DONE);
+
+  for (T j{}; j != n; ++j)
+  {
+    switch (r = exec(stmt))
+    {
+      case SQLITE_ROW:
+        c.emplace(get<typename C::value_type>(stmt, i));
+
+        continue;
+
+      case SQLITE_DONE:;
+        break;
+
+      default:
+        assert(!"unhandled result from exec");
+    }
+
+    break;
+  }
+
+  return r;
+}
+
+//emplace_back////////////////////////////////////////////////////////////////
+template <typename C>
+inline auto emplace_back(stmt_t const& stmt, C& c, int const i = 0)
+{
+  decltype(exec(stmt)) r(SQLITE_DONE);
+
+  for (;;)
+  {
+    switch (r = exec(stmt))
     {
       case SQLITE_ROW:
         c.emplace_back(get<typename C::value_type>(stmt, i));
@@ -720,6 +778,8 @@ inline void emplace_back(stmt_t const& stmt, C& c, int const i = 0)
 
     break;
   }
+
+  return r;
 }
 
 //emplace_back_n//////////////////////////////////////////////////////////////
@@ -727,9 +787,11 @@ template <typename C, typename T>
 inline bool emplace_back_n(stmt_t const& stmt, C& c, T const n,
   int const i = 0)
 {
+  decltype(exec(stmt)) r(SQLITE_DONE);
+
   for (T j{}; j != n; ++j)
   {
-    switch (exec(stmt))
+    switch (r = exec(stmt))
     {
       case SQLITE_ROW:
         c.emplace_back(get<typename C::value_type>(stmt, i));
@@ -737,32 +799,6 @@ inline bool emplace_back_n(stmt_t const& stmt, C& c, T const n,
         continue;
 
       case SQLITE_DONE:;
-        return false;
-
-      default:
-        assert(!"unhandled result from exec");
-    }
-
-    break;
-  }
-
-  return true;
-}
-
-//emplace/////////////////////////////////////////////////////////////////////
-template <typename C>
-inline void emplace(stmt_t const& stmt, C& c, int const i = 0)
-{
-  for (;;)
-  {
-    switch (exec(stmt))
-    {
-      case SQLITE_ROW:
-        c.emplace(get<typename C::value_type>(stmt, i));
-
-        continue;
-
-      case SQLITE_DONE:
         break;
 
       default:
@@ -771,120 +807,8 @@ inline void emplace(stmt_t const& stmt, C& c, int const i = 0)
 
     break;
   }
-}
 
-//emplace_n///////////////////////////////////////////////////////////////////
-template <typename C, typename T>
-inline bool emplace_n(stmt_t const& stmt, C& c, T const n, int const i = 0)
-{
-  for (T j{}; j != n; ++j)
-  {
-    switch (exec(stmt))
-    {
-      case SQLITE_ROW:
-        c.emplace(get<typename C::value_type>(stmt, i));
-
-        continue;
-
-      case SQLITE_DONE:;
-        return false;
-
-      default:
-        assert(!"unhandled result from exec");
-    }
-
-    break;
-  }
-
-  return true;
-}
-
-//|operators//////////////////////////////////////////////////////////////////
-namespace
-{
-  template <typename C>
-  auto emplace_back_(stmt_t const& stmt, void* p, int const i)
-  {
-    return emplace_back(stmt, *static_cast<C*>(p), i);
-  }
-}
-
-class emplace_backer
-{
-  bool (* const f_)(stmt_t const&, void*, int);
-
-  int const i_;
-
-  void* const p_;
-
-public:
-  template <class C>
-  constexpr explicit emplace_backer(C& c, decltype(i_) i = 0) noexcept :
-    f_(emplace_back_<C>),
-    i_(i),
-    p_(&c)
-  {
-  }
-
-  constexpr auto f() const { return f_; }
-
-  constexpr auto i() const { return i_; }
-
-  constexpr auto p() const { return p_; }
-};
-
-template <typename ...A>
-auto operator|(stmt_t const& stmt, emplace_backer const& e)
-{
-  assert(stmt);
-  return e.f()(stmt, e.p(), e.i());
-}
-
-namespace
-{
-  template <typename C, typename T>
-  bool emplace_back_n_(stmt_t const& stmt, void* p, ::std::size_t const n,
-    int const i)
-  {
-    return emplace_back_n(stmt, *static_cast<C*>(p), T(n), i);
-  }
-}
-
-class emplace_backer_n
-{
-  bool (* const f_)(stmt_t const&, void*, ::std::size_t, int);
-
-  ::std::size_t const n_;
-
-  int const i_;
-
-  void* const p_;
-
-public:
-  template <class C, typename T>
-  constexpr explicit emplace_backer_n(C& c, T const n,
-    decltype(i_) i = 0) noexcept :
-    f_(emplace_back_n_<C, T>),
-    n_(n),
-    i_(i),
-    p_(&c)
-  {
-  }
-
-  constexpr auto f() const { return f_; }
-
-  constexpr auto n() const { return n_; }
-
-  constexpr auto i() const { return i_; }
-
-  constexpr auto p() const { return p_; }
-};
-
-template <typename ...A>
-inline auto operator|(stmt_t const& stmt, emplace_backer_n const& e)
-{
-  assert(stmt);
-  return e.f()(stmt, e.p(), e.n(), e.i());
+  return r;
 }
 
 }
