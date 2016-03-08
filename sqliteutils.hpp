c08d1e7ec914af74cec1bf736a4653ba3687af91
janezz55 <janezz55@gmail.com>
2016-03-08 09:50:57 +0100

    some fixes

--- sqliteutils.hpp
+++ sqliteutils.hpp
@@ -618,9 +618,7 @@ inline auto rexecget(S&& stmt, int const i = 0, A&& ...args)
   return get<T>(stmt, i);
 }
 
-template <typename T, int I = 1, typename D, typename A, typename ...B, 
-  typename = typename ::std::enable_if<is_db_t<D>{}>::type
->
+template <typename T, int I = 1, typename D, typename A, typename ...B>
 inline auto execget(D&& db, A&& a, int const i = 0, B&& ...args)
 {
   return execget<T, I>(
